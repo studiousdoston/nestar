6fd86031162fb422d815182c388fb5a0eb58302f
DEEN <studiousdoston@gmail.com>
2026-08-13 10:33:19 +0900

    feat: create Schema Models and project enums

---
+++ apps/nestar-api/src/libs/enums/member.enum.ts
@@ -0,0 +1,17 @@
+export enum MemberType {
+  USER = 'USER',
+  ADMIN = 'ADMIN',
+  AGENT = 'AGENT',
+}
+
+export enum MemberStatus {
+  ACTIVE = 'ACTIVE',
+  BLOCK = 'BLOCK',
+  DELETE = 'DELETE',
+}
+
+export enum MemberAuthType {
+  EMAIL = 'EMAIL',
+  PHONE = 'PHONE',
+  TELEGRAM = 'TELEGRAM',
+}
