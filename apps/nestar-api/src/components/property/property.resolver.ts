f80c0c3c5bb340769b40a215b3c49f6c5c258ac2
DEEN <studiousdoston@gmail.com>
2026-08-31 16:24:45 +0900

    feat:develop updatelPropertyByAdmin graphql api

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -92,4 +92,14 @@ export class PropertyResolver {
     console.log('Query: getAllPropertiesByAdmin');
     return await this.propertyService.getAllPropertiesByAdmin(input);
   }
+
+  //! ---- UPDATE_PROPERTIES_BY_ADMIN -----
+  @Roles(MemberType.ADMIN)
+  @UseGuards(RolesGuard)
+  @Mutation((returns) => Property)
+  public async updatePropertyByAdmin(@Args('input') input: PropertyUpdate): Promise<Property> {
+    console.log('Mutation: updatePropertyByAdmin');
+    input._id = shapeIntoMongoObjectId(input._id);
+    return await this.propertyService.updatePropertyByAdmin(input);
+  }
 }
