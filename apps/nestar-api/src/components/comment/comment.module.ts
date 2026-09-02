5008d2947511618f20e2c97c9f6fdac4fdd79801
DEEN <studiousdoston@gmail.com>
2026-09-02 11:27:53 +0900

    feat: configure comment module & create comment related resolver and service model

--- apps/nestar-api/src/components/comment/comment.module.ts
+++ apps/nestar-api/src/components/comment/comment.module.ts
@@ -1,4 +1,26 @@
 import { Module } from '@nestjs/common';
+import { CommentResolver } from './comment.resolver';
+import { CommentService } from './comment.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import PropertySchema from '../../schemas/Property.model';
+import { AuthModule } from '../auth/auth.module';
+import { MemberModule } from '../member/member.module';
+import { PropertyModule } from '../property/property.module';
+import { BoardArticleModule } from '../board-article/board-article.module';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: 'Comment',
+        schema: PropertySchema,
+      },
+    ]),
+    AuthModule,
+    MemberModule,
+    PropertyModule,
+    BoardArticleModule,
+  ],
+  providers: [CommentResolver, CommentService],
+})
 export class CommentModule {}
--- apps/nestar-api/src/libs/dto/comment/comment.ts
+++ apps/nestar-api/src/libs/dto/comment/comment.ts
@@ -1,45 +1,45 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
-import { ObjectId } from 'mongoose';
+import type { ObjectId } from 'mongoose';
 import { CommentGroup, CommentStatus } from '../../enums/comment.enum';
 import { Member, TotalCounter } from '../member/member';
 
 @ObjectType()
 export class Comment {
-	@Field(() => String)
-	_id: ObjectId;
+  @Field(() => String)
+  _id!: ObjectId;
 
-	@Field(() => CommentStatus)
-	commentStatus: CommentStatus;
+  @Field(() => CommentStatus)
+  commentStatus!: CommentStatus;
 
-	@Field(() => CommentGroup)
-	commentGroup: CommentGroup;
+  @Field(() => CommentGroup)
+  commentGroup!: CommentGroup;
 
-	@Field(() => String)
-	commentContent: string;
+  @Field(() => String)
+  commentContent!: string;
 
-	@Field(() => String)
-	commentRefId: ObjectId;
+  @Field(() => String)
+  commentRefId!: ObjectId;
 
-	@Field(() => String)
-	memberId: ObjectId;
+  @Field(() => String)
+  memberId!: ObjectId;
 
-	@Field(() => Date)
-	createdAt: Date;
+  @Field(() => Date)
+  createdAt!: Date;
 
-	@Field(() => Date)
-	updatedAt: Date;
+  @Field(() => Date)
+  updatedAt!: Date;
 
-	/** from aggregation **/
+  /** from aggregation **/
 
-	@Field(() => Member, { nullable: true })
-	memberData?: Member;
+  @Field(() => Member, { nullable: true })
+  memberData?: Member;
 }
 
 @ObjectType()
 export class Comments {
-	@Field(() => [Comment])
-	list: Comment[];
+  @Field(() => [Comment])
+  list!: Comment[];
 
-	@Field(() => [TotalCounter], { nullable: true })
-	metaCounter: TotalCounter[];
+  @Field(() => [TotalCounter], { nullable: true })
+  metaCounter!: TotalCounter[];
 }
