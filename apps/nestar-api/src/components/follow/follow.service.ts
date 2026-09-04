213e00a14b786a1c1ef06a89beec490e1a5e0c9b
DEEN <studiousdoston@gmail.com>
2026-09-04 15:34:32 +0900

    feat: develop  subscribe, unsubscribe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/components/follow/follow.service.ts
+++ apps/nestar-api/src/components/follow/follow.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Follower, Following } from '../../libs/dto/follow/follow';
+import { Model, ObjectId } from 'mongoose';
+import { Follower, Followers, Following, Followings } from '../../libs/dto/follow/follow';
 import { MemberService } from '../member/member.service';
+import { Direction, Message } from '../../libs/enums/common.enum';
+import { Member } from '../../libs/dto/member/member';
+import { lookupFollowerData, lookupFollowingData } from '../../libs/config';
+import { FollowInquiry } from '../../libs/dto/follow/follow.input';
+import { T } from '../../libs/types/common';
 
 @Injectable()
 export class FollowService {
@@ -11,5 +16,111 @@ export class FollowService {
     private readonly memberService: MemberService,
   ) {}
 
-  
+  //* ---- SUBSCRIBE -----
+  public async subscribe(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+    if (followerId.toString() === followingId.toString()) {
+      throw new InternalServerErrorException(Message.SELF_SUBSCRIPTION_DENIED);
+    }
+
+    const targetMember = await this.memberService.getMember(null, followingId);
+    if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    const result = await this.registerSubscription(followerId, followingId);
+
+    await this.memberService.memberStatsEditor({ _id: followerId, targetKey: 'memberFollowings', modifier: 1 });
+    await this.memberService.memberStatsEditor({ _id: followingId, targetKey: 'memberFollowers', modifier: 1 });
+
+    return result;
+  }
+
+  //* ---- REGISTER_SUBSCRIPTION -----
+  private async registerSubscription(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+    try {
+      return await this.followModel.create({ followingId, followerId });
+    } catch (err) {
+      console.log('ERROR, Service.model:', err instanceof Error ? err.message : err);
+      throw new BadRequestException(Message.CREATE_FAILED);
+    }
+  }
+
+  //* ---- UNSUBSCRIBE -----
+  public async unsubscribe(followerId: ObjectId, followingId: ObjectId): Promise<Follower> {
+    const targetMember = await this.memberService.getMember(null, followingId);
+    if (!targetMember) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    const result = await this.followModel.findOneAndDelete({
+      followingId,
+      followerId,
+    });
+    if (!result) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    await this.memberService.memberStatsEditor({ _id: followerId, targetKey: 'memberFollowings', modifier: -1 });
+    await this.memberService.memberStatsEditor({ _id: followingId, targetKey: 'memberFollowers', modifier: -1 });
+
+    return result;
+  }
+
+  //* ---- GET_MEMBER_FOLLOWINGS -----
+  public async getMemberFollowings(memberId: ObjectId, input: FollowInquiry): Promise<Followings> {
+    const { page, limit, search } = input;
+    if (!search?.followerId) throw new InternalServerErrorException(Message.BAD_REQUEST);
+    const match: T = { followerId: search?.followerId };
+    console.log('match:', match);
+
+    const result = await this.followModel
+      .aggregate([
+        { $match: match },
+        { $sort: { createdAt: Direction.DESC } },
+        {
+          $facet: {
+            list: [
+              { $skip: (page - 1) * limit },
+              { $limit: limit },
+              // meLiked
+              // meFollowed
+              lookupFollowingData,
+
+              { $unwind: '$followingData' },
+            ],
+            metaCounter: [{ $count: 'total' }],
+          },
+        },
+      ])
+      .exec();
+    if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    return result[0];
+  }
+
+  //* ---- GET_MEMBER_FOLLOWERS -----
+  public async getMemberFollowers(memberId: ObjectId, input: FollowInquiry): Promise<Followers> {
+    const { page, limit, search } = input;
+    if (!search?.followingId) throw new InternalServerErrorException(Message.BAD_REQUEST);
+
+    const match: T = { followingId: search?.followingId };
+    console.log('match:', match);
+
+    const result = await this.followModel
+      .aggregate([
+        { $match: match },
+        { $sort: { createdAt: Direction.DESC } },
+        {
+          $facet: {
+            list: [
+              { $skip: (page - 1) * limit },
+              { $limit: limit },
+              // meLiked
+              // meFollowed
+              lookupFollowerData,
+              { $unwind: '$followerData' },
+            ],
+            metaCounter: [{ $count: 'total' }],
+          },
+        },
+      ])
+      .exec();
+    if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+    return result[0];
+  }
 }
--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -3,11 +3,8 @@ import { v4 as uuidv4 } from 'uuid';
 import * as path from 'path';
 
 export const availableAgentSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank'];
-
 export const availableMemberSorts = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews'];
-
 export const availableCommentSorts = ['createdAt', 'updatedAt'];
-
 export const availableOptions = ['propertyBarter', 'propertyRent'];
 export const availablePropertySorts = [
   'createdAt',
@@ -32,12 +29,32 @@ export const getSerialForImage = (filename: string) => {
 export const shapeIntoMongoObjectId = (target: any) => {
   return typeof target === 'string' ? new ObjectId(target) : target;
 };
-
+//-------------------------------------------------------------
+//*                    LOOKUP LOGICS
+//-------------------------------------------------------------
 export const lookupMember = {
   $lookup: {
     from: 'members',
     localField: 'memberId',
     foreignField: '_id',
     as: 'memberData',
+  },
+};
+
+export const lookupFollowingData = {
+  $lookup: {
+    from: 'members',
+    localField: 'followingId',
+    foreignField: '_id',
+    as: 'followingData',
+  },
+};
+
+export const lookupFollowerData = {
+  $lookup: {
+    from: 'members',
+    localField: 'followerId',
+    foreignField: '_id',
+    as: 'followerData',
   },
 };
